e9bac2b7beb6d8a135ecef6f8a8c019eb5e60c30
Dylan Pierce <dylanpierce914@gmail.com>
2019-02-16 13:09:12 -0800

    Implementing abstract class, except 1 method

--- src/Provider/Coinbase.php
+++ src/Provider/Coinbase.php
@@ -8,19 +8,60 @@ use \League\OAuth2\Client\Token\AccessToken;
 
 class Coinbase extends AbstractProvider {
 
-  public function urlAuthorize() {
-    return "https://www.coinbase.com/oauth/authorize";
-  }
+    /**
+     * Returns the base URL for authorizing a client.
+     *
+     * Eg. https://oauth.service.com/authorize
+     *
+     * @return string
+     */
+    public function getBaseAuthorizationUrl() {
+      return "https://www.coinbase.com/oauth/authorize";
+    }
 
-  public function urlAccessToken() {
-    return "https://www.coinbase.com/oauth/token";
-  }
+    /**
+     * Returns the base URL for requesting an access token.
+     *
+     * Eg. https://oauth.service.com/token
+     *
+     * @param array $params
+     * @return string
+     */
+    public function getBaseAccessTokenUrl(array $params) {
+      return "https://www.coinbase.com/oauth/token";
+    }
 
-  public function urlUserDetails(AccessToken $token) {
-    return "https://api.coinbase.com/v2/user";
-  }
+    /**
+     * Checks a provider response for errors.
+     *
+     * @throws IdentityProviderException
+     * @param  ResponseInterface $response
+     * @param  array|string $data Parsed response data
+     * @return void
+     */
+    protected function checkResponse(ResponseInterface $response, $data) {
+      dd([$response, $data]);
+    }
 
-  public function userDetails($response, AccessToken $token) {
-    return $response;
-  }
+    /**
+     * Generates a resource owner object from a successful resource owner
+     * details request.
+     *
+     * @param  array $response
+     * @param  AccessToken $token
+     * @return ResourceOwnerInterface
+     */
+    protected function createResourceOwner(array $response, AccessToken $token) {
+
+    }
+
+    /**
+     * Returns the URL for requesting the resource owner's details.
+     *
+     * @param AccessToken $token
+     * @return string
+     */
+    public function getResourceOwnerDetailsUrl(AccessToken $token) {
+      return "https://api.coinbase.com/v2/user";
+    }
 }
